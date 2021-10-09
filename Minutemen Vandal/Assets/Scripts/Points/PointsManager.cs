802f7cf30b07f7119a793996c3d800e0df5bf79e
Ferid <ferid.dragon@hotmail.com>
2021-10-10 01:35:37 +0200

    Handled calculating intersection volume for spheres which are not intersecting.

--- Minutemen Vandal/Assets/Scripts/Points/PointsManager.cs
+++ Minutemen Vandal/Assets/Scripts/Points/PointsManager.cs
@@ -61,6 +61,11 @@ public class PointsManager : MonoBehaviour
     /// <returns></returns>
     private float SphereIntersectionVolume(float r, float R, float d)
     {
+        if (r + R < d)
+        {
+            // If sum of radii is greater than center distance, the spheres are not intersecting => intersection volume is 0.
+            return 0;
+        }
         return Mathf.PI * Mathf.Pow(R + r - d, 2) * (Mathf.Pow(d, 2) + 2 * d * r - 3 * Mathf.Pow(r, 2) + 2 * d * R - 3 * Mathf.Pow(R, 2) + 6 * r * R) / (12 * d);
     }
 
